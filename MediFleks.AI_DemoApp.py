85c50028527d72b0fde0d0f25393372c74b557ec
BDCIMERIGit <bdc.imeri-idealab@ui.ac.id>
2025-07-03 21:16:22 +0700

    MediFleks.AI_DemoApp.py

--- MediFleks.AI_DemoApp.py
+++ MediFleks.AI_DemoApp.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import requests
+import joblib
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import LabelEncoder, StandardScaler
